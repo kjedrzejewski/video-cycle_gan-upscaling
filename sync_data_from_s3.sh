10c5291148f5efdfe229e3ded57dab627e674655
Krzysztof Jędrzejewski <kjedrz@gmail.com>
2019-09-23 03:07:51 +0200

    sync improvement

--- sync_data_from_s3.sh
+++ sync_data_from_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-aws s3 sync s3://prd-prsn-er-pet-projects/upscaling/example_outputs upscaling/example_images
-aws s3 sync s3://prd-prsn-er-pet-projects/upscaling/models upscaling/trained_model
-aws s3 sync s3://prd-prsn-er-pet-projects/upscaling/images images
+aws s3 sync --exact-timestamps s3://prd-prsn-er-pet-projects/upscaling/example_outputs upscaling/example_images
+aws s3 sync --exact-timestamps s3://prd-prsn-er-pet-projects/upscaling/models upscaling/trained_model
+aws s3 sync --exact-timestamps s3://prd-prsn-er-pet-projects/upscaling/images images
