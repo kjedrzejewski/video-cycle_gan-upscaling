8a9a9ba734a3c8ce71e1d3d4e8e67278d3924ba2
Krzysztof Jędrzejewski <kjedrz@gmail.com>
2019-10-12 13:22:50 +0200

    losses

--- sync_data_to_s3.sh
+++ sync_data_to_s3.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 aws s3 sync --exact-timestamps upscaling/example_images s3://prd-prsn-er-pet-projects/upscaling/example_outputs
+aws s3 sync --exact-timestamps upscaling/losses s3://prd-prsn-er-pet-projects/upscaling/losses
 aws s3 sync --exact-timestamps upscaling/trained_model s3://prd-prsn-er-pet-projects/upscaling/models
 aws s3 sync --exact-timestamps images s3://prd-prsn-er-pet-projects/upscaling/images
